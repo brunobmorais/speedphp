dd9149297c8ccaf195ad7db09f18e81aaf446e77
Bruno Morais <bruno@MBP13.local>
2024-03-02 23:01:34 -0300

    correcao

--- docker/config/mysql/dump/database.sql
+++ docker/config/mysql/dump/database.sql
@@ -1,141 +1,124 @@
--- MySQL dump 10.13  Distrib 8.0.36, for macos14 (arm64)
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost    Database: database
--- ------------------------------------------------------
--- Server version	8.2.0
+-- Host: mysql
+-- Tempo de geração: 03/03/2024 às 01:40
+-- Versão do servidor: 8.2.0
+-- Versão do PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET NAMES utf8mb4 */;
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `MODULO`
+-- Estrutura para tabela `MODULO`
 --
 
-DROP TABLE IF EXISTS `MODULO`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `MODULO` (
-  `CODMODULO` int NOT NULL AUTO_INCREMENT,
+  `CODMODULO` int NOT NULL,
   `TITULO` varchar(100) DEFAULT NULL,
   `DESCRICAO` varchar(200) DEFAULT NULL,
   `ICONE` varchar(100) DEFAULT NULL,
   `CONTROLLER` varchar(200) DEFAULT NULL,
   `ORDEM` int DEFAULT '1',
   `SITUACAO` int DEFAULT '1',
-  `EXCLUIDO` int NOT NULL DEFAULT '0',
-  PRIMARY KEY (`CODMODULO`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb3;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `EXCLUIDO` int NOT NULL DEFAULT '0'
+);
 
 --
--- Dumping data for table `MODULO`
+-- Despejando dados para a tabela `MODULO`
 --
 
-LOCK TABLES `MODULO` WRITE;
-/*!40000 ALTER TABLE `MODULO` DISABLE KEYS */;
-INSERT INTO `MODULO` VALUES (1,'Configurações','Cadastro de informações base do sistema','mdi-cog-outline','configuracoes',1,1,0),(2,'Módulo 2','Teste','mdi-account-outline','tete',1,1,0),(3,'Módulo 3','Teste','mdi-account-outline','teste',1,1,0),(4,'Módulo 4','Teste','mdi-account-outline','tete',1,1,0),(5,'Gestão de Pessoas','Cadastro de funcionários','mdi-account-group','gestaopessoas',1,1,0);
-/*!40000 ALTER TABLE `MODULO` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `MODULO` (`CODMODULO`, `TITULO`, `DESCRICAO`, `ICONE`, `CONTROLLER`, `ORDEM`, `SITUACAO`, `EXCLUIDO`) VALUES
+(1, 'Configurações', 'Cadastro de informações base do sistema', 'mdi-cog-outline', 'configuracoes', 1, 1, 0),
+(2, 'Módulo 2', 'Teste', 'mdi-account-outline', 'tete', 1, 1, 0),
+(3, 'Módulo 3', 'Teste', 'mdi-account-outline', 'teste', 1, 1, 0),
+(4, 'Módulo 4', 'Teste', 'mdi-account-outline', 'tete', 1, 1, 0),
+(5, 'Gestão de Pessoas', 'Cadastro de funcionários', 'mdi-account-group', 'gestaopessoas', 1, 1, 0);
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `PERFIL`
+-- Estrutura para tabela `PERFIL`
 --
 
-DROP TABLE IF EXISTS `PERFIL`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `PERFIL` (
-  `CODPERFIL` int NOT NULL AUTO_INCREMENT,
+  `CODPERFIL` int NOT NULL,
   `NOME` varchar(100) NOT NULL,
   `NIVEL` int NOT NULL DEFAULT '10',
-  `EXCLUIDO` int NOT NULL DEFAULT '0',
-  PRIMARY KEY (`CODPERFIL`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `EXCLUIDO` int NOT NULL DEFAULT '0'
+) ;
 
 --
--- Dumping data for table `PERFIL`
+-- Despejando dados para a tabela `PERFIL`
 --
 
-LOCK TABLES `PERFIL` WRITE;
-/*!40000 ALTER TABLE `PERFIL` DISABLE KEYS */;
-INSERT INTO `PERFIL` VALUES (1,'EXTERNO',1,0),(2,'TÉCNICO',10,0),(3,'COORDENADOR',20,0),(4,'GERENTE',30,0),(5,'DIRETOR',40,0),(6,'PRESIDENTE',50,0);
-/*!40000 ALTER TABLE `PERFIL` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `PERFIL` (`CODPERFIL`, `NOME`, `NIVEL`, `EXCLUIDO`) VALUES
+(1, 'EXTERNO', 1, 0),
+(2, 'TÉCNICO', 10, 0),
+(3, 'COORDENADOR', 20, 0),
+(4, 'GERENTE', 30, 0),
+(5, 'DIRETOR', 40, 0),
+(6, 'PRESIDENTE', 50, 0);
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `PERFIL_USUARIO`
+-- Estrutura para tabela `PERFIL_USUARIO`
 --
 
-DROP TABLE IF EXISTS `PERFIL_USUARIO`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `PERFIL_USUARIO` (
-  `CODPERFIL_USUARIO` int NOT NULL AUTO_INCREMENT,
+  `CODPERFIL_USUARIO` int NOT NULL,
   `CODUSUARIO` int NOT NULL,
   `CODPERFIL` int NOT NULL,
-  `EXCLUIDO` int NOT NULL DEFAULT '0',
-  PRIMARY KEY (`CODPERFIL_USUARIO`),
-  KEY `fk_PERFIL_USUARIO_USUARIO1_idx` (`CODUSUARIO`),
-  KEY `fk_PERFIL_USUARIO_PERFIL1_idx` (`CODPERFIL`),
-  CONSTRAINT `fk_PERFIL_USUARIO_USUARIO1` FOREIGN KEY (`CODUSUARIO`) REFERENCES `USUARIO` (`CODUSUARIO`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `EXCLUIDO` int NOT NULL DEFAULT '0'
+) ;
 
 --
--- Dumping data for table `PERFIL_USUARIO`
+-- Despejando dados para a tabela `PERFIL_USUARIO`
 --
 
-LOCK TABLES `PERFIL_USUARIO` WRITE;
-/*!40000 ALTER TABLE `PERFIL_USUARIO` DISABLE KEYS */;
-INSERT INTO `PERFIL_USUARIO` VALUES (1,1,6,0);
-/*!40000 ALTER TABLE `PERFIL_USUARIO` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `PERFIL_USUARIO` (`CODPERFIL_USUARIO`, `CODUSUARIO`, `CODPERFIL`, `EXCLUIDO`) VALUES
+(1, 1, 6, 0);
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `PESSOA`
+-- Estrutura para tabela `PESSOA`
 --
 
-DROP TABLE IF EXISTS `PESSOA`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `PESSOA` (
-  `CODPESSOA` int NOT NULL AUTO_INCREMENT,
+  `CODPESSOA` int NOT NULL,
   `NOME` varchar(200) DEFAULT NULL,
   `CPF` varchar(45) DEFAULT NULL,
   `EMAIL` varchar(200) DEFAULT NULL,
-  `EXCLUIDO` int NOT NULL DEFAULT '0',
-  PRIMARY KEY (`CODPESSOA`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `EXCLUIDO` int NOT NULL DEFAULT '0'
+);
 
 --
--- Dumping data for table `PESSOA`
+-- Despejando dados para a tabela `PESSOA`
 --
 
-LOCK TABLES `PESSOA` WRITE;
-/*!40000 ALTER TABLE `PESSOA` DISABLE KEYS */;
-INSERT INTO `PESSOA` VALUES (1,'Usuario Teste','00000000000','brunomoraisti@gmail.com',0);
-/*!40000 ALTER TABLE `PESSOA` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `PESSOA` (`CODPESSOA`, `NOME`, `CPF`, `EMAIL`, `EXCLUIDO`) VALUES
+(1, 'Usuario Teste', '00000000000', 'brunomoraisti@gmail.com', 0);
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `PRIVILEGIO`
+-- Estrutura para tabela `PRIVILEGIO`
 --
 
-DROP TABLE IF EXISTS `PRIVILEGIO`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `PRIVILEGIO` (
-  `CODPRIVILEGIO` int NOT NULL AUTO_INCREMENT,
+  `CODPRIVILEGIO` int NOT NULL,
   `CODSERVICO` int NOT NULL,
   `CODPERFIL` int NOT NULL,
   `EXCLUIR` int NOT NULL DEFAULT '0',
@@ -143,33 +126,28 @@ CREATE TABLE `PRIVILEGIO` (
   `SALVAR` int NOT NULL DEFAULT '0',
   `ALTERAR` int NOT NULL DEFAULT '0',
   `OUTROS` int NOT NULL DEFAULT '0',
-  `EXCLUIDO` int NOT NULL DEFAULT '0',
-  PRIMARY KEY (`CODPRIVILEGIO`),
-  KEY `FK_PRIVILEGIO_SERVICO_idx` (`CODSERVICO`),
-  KEY `fk_PRIVILEGIO_PERFIL1_idx` (`CODPERFIL`),
-  CONSTRAINT `FK_PRIVILEGIO_SERVICO` FOREIGN KEY (`CODSERVICO`) REFERENCES `SERVICO` (`CODSERVICO`)
-) ENGINE=InnoDB AUTO_INCREMENT=67 DEFAULT CHARSET=utf8mb3;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `EXCLUIDO` int NOT NULL DEFAULT '0'
+);
 
 --
--- Dumping data for table `PRIVILEGIO`
+-- Despejando dados para a tabela `PRIVILEGIO`
 --
 
-LOCK TABLES `PRIVILEGIO` WRITE;
-/*!40000 ALTER TABLE `PRIVILEGIO` DISABLE KEYS */;
-INSERT INTO `PRIVILEGIO` VALUES (62,1,6,1,1,1,1,1,0),(63,2,6,1,1,1,1,1,0),(64,3,6,1,1,1,1,1,0),(65,4,6,1,1,1,1,1,0),(66,5,6,1,1,1,1,1,0);
-/*!40000 ALTER TABLE `PRIVILEGIO` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `PRIVILEGIO` (`CODPRIVILEGIO`, `CODSERVICO`, `CODPERFIL`, `EXCLUIR`, `LER`, `SALVAR`, `ALTERAR`, `OUTROS`, `EXCLUIDO`) VALUES
+(62, 1, 6, 1, 1, 1, 1, 1, 0),
+(63, 2, 6, 1, 1, 1, 1, 1, 0),
+(64, 3, 6, 1, 1, 1, 1, 1, 0),
+(65, 4, 6, 1, 1, 1, 1, 1, 0),
+(66, 5, 6, 1, 1, 1, 1, 1, 0);
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `SERVICO`
+-- Estrutura para tabela `SERVICO`
 --
 
-DROP TABLE IF EXISTS `SERVICO`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `SERVICO` (
-  `CODSERVICO` int NOT NULL AUTO_INCREMENT,
+  `CODSERVICO` int NOT NULL,
   `CODMODULO` int DEFAULT NULL,
   `TITULO` varchar(100) DEFAULT NULL,
   `DESCRICAO` varchar(200) DEFAULT NULL,
@@ -177,60 +155,171 @@ CREATE TABLE `SERVICO` (
   `CONTROLLER` varchar(100) DEFAULT NULL,
   `ORDEM` int NOT NULL DEFAULT '1',
   `SITUACAO` int NOT NULL DEFAULT '1',
-  `EXCLUIDO` int NOT NULL DEFAULT '0',
-  PRIMARY KEY (`CODSERVICO`),
-  KEY `FK_SERVICO_MODULO_idx` (`CODMODULO`),
-  CONSTRAINT `FK_SERVICO_MODULO` FOREIGN KEY (`CODMODULO`) REFERENCES `MODULO` (`CODMODULO`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb3;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `EXCLUIDO` int NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 --
--- Dumping data for table `SERVICO`
+-- Despejando dados para a tabela `SERVICO`
 --
 
-LOCK TABLES `SERVICO` WRITE;
-/*!40000 ALTER TABLE `SERVICO` DISABLE KEYS */;
-INSERT INTO `SERVICO` VALUES (1,1,'Módulos','modulos','mdi mdi-archive-plus-outline','modulos',1,1,0),(2,1,'Serviços','Serviços','mdi mdi-archive-plus-outline','servicos',1,1,0),(3,1,'Usuários','Usuários do Sistema','mdi mdi-archive-plus-outline','usuarios',1,1,0),(4,5,'Funcionários','Cadastro de funcionários','mdi-account','funcionarios',1,1,0),(5,1,'Perfils','Pefil de usuários','mdi mdi-archive-plus-outline','perfil',1,1,0);
-/*!40000 ALTER TABLE `SERVICO` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `SERVICO` (`CODSERVICO`, `CODMODULO`, `TITULO`, `DESCRICAO`, `ICONE`, `CONTROLLER`, `ORDEM`, `SITUACAO`, `EXCLUIDO`) VALUES
+(1, 1, 'Módulos', 'modulos', 'mdi mdi-archive-plus-outline', 'modulos', 1, 1, 0),
+(2, 1, 'Serviços', 'Serviços', 'mdi mdi-archive-plus-outline', 'servicos', 1, 1, 0),
+(3, 1, 'Usuários', 'Usuários do Sistema', 'mdi mdi-archive-plus-outline', 'usuarios', 1, 1, 0),
+(4, 5, 'Funcionários', 'Cadastro de funcionários', 'mdi-account', 'funcionarios', 1, 1, 0),
+(5, 1, 'Perfils', 'Pefil de usuários', 'mdi mdi-archive-plus-outline', 'perfil', 1, 1, 0);
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `USUARIO`
+-- Estrutura para tabela `USUARIO`
 --
 
-DROP TABLE IF EXISTS `USUARIO`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `USUARIO` (
-  `CODUSUARIO` int NOT NULL AUTO_INCREMENT,
+  `CODUSUARIO` int NOT NULL,
   `CODPESSOA` int NOT NULL,
   `SENHA` varchar(200) DEFAULT NULL,
   `SITUACAO` int NOT NULL DEFAULT '0',
   `DATACADASTRO` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `EXCLUIDO` int NOT NULL DEFAULT '0',
-  PRIMARY KEY (`CODUSUARIO`),
-  KEY `FK_USUARIO_PESSOA_idx` (`CODPESSOA`),
-  CONSTRAINT `FK_USUARIO_PESSOA` FOREIGN KEY (`CODPESSOA`) REFERENCES `PESSOA` (`CODPESSOA`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `EXCLUIDO` int NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 --
--- Dumping data for table `USUARIO`
+-- Despejando dados para a tabela `USUARIO`
 --
 
-LOCK TABLES `USUARIO` WRITE;
-/*!40000 ALTER TABLE `USUARIO` DISABLE KEYS */;
-INSERT INTO `USUARIO` VALUES (1,1,'$2y$10$H92r2Spv1CAI4Gqu.d0xRO9eJsxbeskj8QyuRCHypq9aRtjCWn8M.',1,'2023-01-01 00:00:00',0);
-/*!40000 ALTER TABLE `USUARIO` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+INSERT INTO `USUARIO` (`CODUSUARIO`, `CODPESSOA`, `SENHA`, `SITUACAO`, `DATACADASTRO`, `EXCLUIDO`) VALUES
+(1, 1, '$2y$10$H92r2Spv1CAI4Gqu.d0xRO9eJsxbeskj8QyuRCHypq9aRtjCWn8M.', 1, '2023-01-01 00:00:00', 0);
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `MODULO`
+--
+ALTER TABLE `MODULO`
+  ADD PRIMARY KEY (`CODMODULO`);
+
+--
+-- Índices de tabela `PERFIL`
+--
+ALTER TABLE `PERFIL`
+  ADD PRIMARY KEY (`CODPERFIL`);
+
+--
+-- Índices de tabela `PERFIL_USUARIO`
+--
+ALTER TABLE `PERFIL_USUARIO`
+  ADD PRIMARY KEY (`CODPERFIL_USUARIO`),
+  ADD KEY `fk_PERFIL_USUARIO_USUARIO1_idx` (`CODUSUARIO`),
+  ADD KEY `fk_PERFIL_USUARIO_PERFIL1_idx` (`CODPERFIL`);
+
+--
+-- Índices de tabela `PESSOA`
+--
+ALTER TABLE `PESSOA`
+  ADD PRIMARY KEY (`CODPESSOA`);
+
+--
+-- Índices de tabela `PRIVILEGIO`
+--
+ALTER TABLE `PRIVILEGIO`
+  ADD PRIMARY KEY (`CODPRIVILEGIO`),
+  ADD KEY `FK_PRIVILEGIO_SERVICO_idx` (`CODSERVICO`),
+  ADD KEY `fk_PRIVILEGIO_PERFIL1_idx` (`CODPERFIL`);
+
+--
+-- Índices de tabela `SERVICO`
+--
+ALTER TABLE `SERVICO`
+  ADD PRIMARY KEY (`CODSERVICO`),
+  ADD KEY `FK_SERVICO_MODULO_idx` (`CODMODULO`);
+
+--
+-- Índices de tabela `USUARIO`
+--
+ALTER TABLE `USUARIO`
+  ADD PRIMARY KEY (`CODUSUARIO`),
+  ADD KEY `FK_USUARIO_PESSOA_idx` (`CODPESSOA`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `MODULO`
+--
+ALTER TABLE `MODULO`
+  MODIFY `CODMODULO` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de tabela `PERFIL`
+--
+ALTER TABLE `PERFIL`
+  MODIFY `CODPERFIL` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de tabela `PERFIL_USUARIO`
+--
+ALTER TABLE `PERFIL_USUARIO`
+  MODIFY `CODPERFIL_USUARIO` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de tabela `PESSOA`
+--
+ALTER TABLE `PESSOA`
+  MODIFY `CODPESSOA` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de tabela `PRIVILEGIO`
+--
+ALTER TABLE `PRIVILEGIO`
+  MODIFY `CODPRIVILEGIO` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=67;
+
+--
+-- AUTO_INCREMENT de tabela `SERVICO`
+--
+ALTER TABLE `SERVICO`
+  MODIFY `CODSERVICO` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de tabela `USUARIO`
+--
+ALTER TABLE `USUARIO`
+  MODIFY `CODUSUARIO` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Restrições para tabelas despejadas
+--
+
+--
+-- Restrições para tabelas `PERFIL_USUARIO`
+--
+ALTER TABLE `PERFIL_USUARIO`
+  ADD CONSTRAINT `fk_PERFIL_USUARIO_PERFIL` FOREIGN KEY (`CODPERFIL`) REFERENCES `PERFIL` (`CODPERFIL`),
+  ADD CONSTRAINT `fk_PERFIL_USUARIO_USUARIO1` FOREIGN KEY (`CODUSUARIO`) REFERENCES `USUARIO` (`CODUSUARIO`);
+
+--
+-- Restrições para tabelas `PRIVILEGIO`
+--
+ALTER TABLE `PRIVILEGIO`
+  ADD CONSTRAINT `FK_PRIVILEGIO_PERFIL` FOREIGN KEY (`CODPERFIL`) REFERENCES `PERFIL` (`CODPERFIL`),
+  ADD CONSTRAINT `FK_PRIVILEGIO_SERVICO` FOREIGN KEY (`CODSERVICO`) REFERENCES `SERVICO` (`CODSERVICO`);
+
+--
+-- Restrições para tabelas `SERVICO`
+--
+ALTER TABLE `SERVICO`
+  ADD CONSTRAINT `FK_SERVICO_MODULO` FOREIGN KEY (`CODMODULO`) REFERENCES `MODULO` (`CODMODULO`);
+
+--
+-- Restrições para tabelas `USUARIO`
+--
+ALTER TABLE `USUARIO`
+  ADD CONSTRAINT `FK_USUARIO_PESSOA` FOREIGN KEY (`CODPESSOA`) REFERENCES `PESSOA` (`CODPESSOA`);
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2024-02-04 10:34:55
